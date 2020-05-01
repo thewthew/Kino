ced5a66ec6d062b667f1314d4da23a76a09b9924
matti <matthew.usdin@telecom-bretagne.eu>
2020-05-01 11:17:19 +0200

    Initial Commit

---
+++ Kino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kino
+//
+//  Created by Matti on 01/05/2020.
+//  Copyright © 2020 Matti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
