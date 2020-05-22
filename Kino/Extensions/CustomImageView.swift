7eadc956c55487dc902587706e2d767fe762346b
matti <matthew.usdin@telecom-bretagne.eu>
2020-05-22 22:51:10 +0200

    replace DispatchQueue to fix laggy scrolling

--- Kino/Extensions/CustomImageView.swift
+++ Kino/Extensions/CustomImageView.swift
@@ -69,20 +69,21 @@ class CustomImageView: UIImageView {
                 return
             }
 
-            DispatchQueue.main.async { [weak self] in
-                guard let data = data, let imageToCache = UIImage(data: data), let self = self else {
-                    return
-                }
+            guard let data = data,
+                let compressedImageData = UIImage(data: data)?.jpegData(compressionQuality: 1),
+                let compressedImage = UIImage(data: compressedImageData) else { return }
                 if self.imageUrlString == urlString {
-                    self.removeAnimation()
-                    UIView.transition(with: self,
-                                      duration: 0.3,
-                                      options: .transitionCrossDissolve,
-                                      animations: { self.image = imageToCache },
-                                      completion: nil)
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self = self else { return }
+                        self.removeAnimation()
+                        UIView.transition(with: self,
+                                          duration: 0.3,
+                                          options: .transitionCrossDissolve,
+                                          animations: { self.image = compressedImage },
+                                          completion: nil)
+                    }
                 }
-                imageCache.setObject(imageToCache, forKey: urlString as AnyObject)
-            }
+            imageCache.setObject(compressedImage, forKey: urlString as AnyObject)
         }).resume()
     }
 }
